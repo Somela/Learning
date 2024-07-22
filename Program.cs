9da0e3ec56baba2e5eaaddc212d1faadcba452f9
Sanjeeva reddy Somela <s.somela@spglobal.com>
2024-07-22 11:35:41 +0530

    Enhanced to use for loop to repeat the operations

--- Program.cs
+++ Program.cs
@@ -11,18 +11,24 @@ namespace Learning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter X Value");
-            int x = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please Enter Y Value");
-            int y = int.Parse(Console.ReadLine());
-            Console.WriteLine("\t"+"1. Addition\n\t2. Substraction\n\t3 .Multiplication\n\t4. Division\n");
-            int z = int.Parse(Console.ReadLine());
-            ArthimaticOperationResponse arthimaticOperationResponse = new ArthimaticOperationResponse();
-            string operation = arthimaticOperationResponse.FindArthimatic(z);
-            Console.WriteLine("Selected Operation is: " + operation);
-            SwitchResponse switchResponse = new SwitchResponse();
-            double result = switchResponse.ArthematicOperation(operation, x, y);
-            Console.WriteLine("Result is: " + result);
+            Console.WriteLine("Please Enter How many times you want to repeat");
+            int repeat = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < repeat; i++)
+            {
+
+                Console.WriteLine("Please Enter X Value");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please Enter Y Value");
+                int y = int.Parse(Console.ReadLine());
+                Console.WriteLine("\t" + "1. Addition\n\t2. Substraction\n\t3 .Multiplication\n\t4. Division\n");
+                int z = int.Parse(Console.ReadLine());
+                ArthimaticOperationResponse arthimaticOperationResponse = new ArthimaticOperationResponse();
+                string operation = arthimaticOperationResponse.FindArthimatic(z);
+                Console.WriteLine("Selected Operation is: " + operation);
+                SwitchResponse switchResponse = new SwitchResponse();
+                double result = switchResponse.ArthematicOperation(operation, x, y);
+                Console.WriteLine("Result is: " + result);
+            }
             Console.ReadKey();
         }
     }
